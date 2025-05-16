442247cf9a0f7865c2a3e818a36f6aba0a138d84
doublealpha21 <praise8fakorede@gmail.com>
2025-05-16 02:44:12 +0100

    Main Root changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:kenkeputa/core/router/routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,13 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    return MaterialApp.router(
+      title: 'Kenkeputa',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
+      routerConfig: router,
     );
   }
 }
